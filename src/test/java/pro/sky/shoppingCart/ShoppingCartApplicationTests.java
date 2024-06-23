d0df0663d7aff6212a74188eabe4cdce9f59fe43
linda <charming.linda85@gmail.com>
2024-06-23 10:06:51 +0300

    Initial commit

---
+++ src/test/java/pro/sky/shoppingCart/ShoppingCartApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.shoppingCart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingCartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
